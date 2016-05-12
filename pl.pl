9a4bc439edd100e624dbaf19ea3a2567ea2c74db
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-12 15:12:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pl.pl
+++ pl.pl
@@ -1,6 +1,11 @@
+author("Dmitry Ponyatov <dponyatov@gmail.com>").
+
+github(X,GitHub) :- 
+	atom_concat('https://github.com/ponyatov/',X,GitHub).
+
 app('decl').			% [app]lication
 
-about("Prolog-powered [decl]arative template translator").
+about('decl',"Prolog-powered [decl]arative template translator").
 
 file('README.md').		% README
 file('Makefile').		% build script
@@ -10,16 +15,23 @@ file('lpp.lpp').		% syntax lexer
 file('hpp.hpp').		% C++ core headers
 file('cpp.cpp').		% C++ core code
 
+readme :-
+	app(Application),about(Application,About),
+	author(Author),github(Application,GitHub),
+	format("# ~s~n### ~s~n~n(c) ~s~n~ngithub: ~s",
+		[Application,About,Author,GitHub]),fail.
+	%atom_concat('# ',App,Readme).
+	
+contents('README.md',X) :- X="".
+
 write_files :- app(Application),file(FileName),
 	atom_concat(Application,'/',AppSl),
 	atom_concat(AppSl,FileName,FullFileName),
-	write(FullFileName),nl,
-%	open(Y,update,Z),close(Z),
+	write("write "),write(FullFileName),nl,
+	open(FullFileName,write,FileHandle),
+		contents(FileName,X),write(FileHandle,X),
+	close(FileHandle),
 	fail.
 	
-readme(Readme) :-
-	app(Application),
-	atom_concat('# ',App,Readme).
-
-%:- initialization(write_files).
-:- initialization(readme(X)).
+:- initialization(write_files).
+%:- initialization(readme).
