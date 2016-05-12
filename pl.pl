f7264bd481e9afb4d4696f18f0cec197381847c4
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-12 12:25:16 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pl.pl
+++ pl.pl
@@ -1,1 +1,15 @@
-:- write('hello').
+file('README.md').		% README
+file('Makefile').		% build script
+file('.gitignore').		% ignore produced & temp files 
+file('ypp.ypp').		% syntax parser for configs & user scripts
+file('lpp.lpp').		% syntax lexer
+file('hpp.hpp').		% C++ core headers
+file('cpp.cpp').		% C++ core code
+
+wr :- file(X),
+	atom_concat('decl/',X,Y),
+	write(Y),nl,
+	open(Y,update,Z),close(Z),
+	fail.
+
+:- initialization(wr).
