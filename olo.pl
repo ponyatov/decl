052c40442a31ddad64b8019e14a0510a1e071fc5
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-13 16:32:33 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- olo.pl
+++ olo.pl
@@ -1,6 +1,17 @@
 % ololo
 
-file('decl/hpp.hpp',Contents) :- swritef(Contents,"",[]).
+lst_to_str([],Y,Y).
+lst_to_str([H|T],Y,T) :-
+	swritef(Y,"%w\n",H), 
+	lst_to_str(T,Y,T).
+
+file('decl/hpp.hpp',Contents) :-
+	lst_to_str([
+		"#ifndef _H_",
+		"#define _H_",
+		"#endif // _H_"
+	],"",Contents).
+
 file('decl/cpp.cpp',Contents) :- swritef(Contents,"#include \"hpp.hpp\"",[]).
 
 write_files :- file(X,Y),open(X,write,F),write(F,Y),close(F),fail.
