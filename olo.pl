9ef34ea7cf9302c863ad359cd9a9d816dc0cb156
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-13 16:15:01 +0400

    + ololo hpp/cpp
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- olo.pl
+++ olo.pl
@@ -1,1 +1,8 @@
 % ololo
+
+file('decl/hpp.hpp',Contents) :- swritef(Contents,"",[]).
+file('decl/cpp.cpp',Contents) :- swritef(Contents,"#include \"hpp.hpp\"",[]).
+
+write_files :- file(X,Y),open(X,write,F),write(F,Y),close(F),fail.
+
+:- initialization(write_files).
