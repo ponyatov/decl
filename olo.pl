37a4964e8d3c7c0e56533ad57016fd72be974985
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-13 17:03:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- olo.pl
+++ olo.pl
@@ -1,19 +1,28 @@
 % ololo
 
-lst_to_str([],Y,Y).
-lst_to_str([H|T],Y,T) :-
-	swritef(Y,"%w\n",H), 
-	lst_to_str(T,Y,T).
+l2s([],L,L).
 
-file('decl/hpp.hpp',Contents) :-
-	lst_to_str([
-		"#ifndef _H_",
-		"#define _H_",
-		"#endif // _H_"
-	],"",Contents).
+%join(_, [X], X) :- !.
+%join(Sep, [X | Xs], S) :-
+%    join(Sep, Xs, Sx),
+%    string_concat(Sep, Sx, Sy),
+%    string_concat(X, Sy, S).
+%atomic_list_concat    
 
-file('decl/cpp.cpp',Contents) :- swritef(Contents,"#include \"hpp.hpp\"",[]).
-
-write_files :- file(X,Y),open(X,write,F),write(F,Y),close(F),fail.
-
-:- initialization(write_files).
+%lst_to_str([],Y,Y).
+%lst_to_str([H|T],Y,T) :-
+%	swritef(Y,"%w\n",H), 
+%	lst_to_str(T,Y,T).
+%
+%file('decl/hpp.hpp',Contents) :-
+%	lst_to_str([
+%		"#ifndef _H_",
+%		"#define _H_",
+%		"#endif // _H_"
+%	],"",Contents).
+%
+%file('decl/cpp.cpp',Contents) :- swritef(Contents,"#include \"hpp.hpp\"",[]).
+%
+%write_files :- file(X,Y),open(X,write,F),write(F,Y),close(F),fail.
+%
+%:- initialization(write_files).
