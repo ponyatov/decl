d8c651c40b494b4591a0948e4a61fafcbd15f4fb
Dmitry Ponyatov <dponyatov@gmail.com>
2026-05-27 14:32:37 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/modbus.py
+++ src/modbus.py
@@ -3,53 +3,78 @@ import datetime as dt
 from pymodbus.transaction import ModbusRtuFramer
 from pymodbus.client import ModbusSerialClient
 
-SERIAL = '/dev/ttyAMA3'
-# SERIAL = '/dev/ttyUSB0'
-BAUDRATE = 115200
 
-icpdas = ModbusSerialClient(
-    port=SERIAL,           # последовательный порт
-    baudrate=BAUDRATE,    # скорость передачи
-    bytesize=8,           # 8 бит данных
-    parity='N',           # без четности
-    stopbits=1,           # 1 стоп-бит
-    framer=ModbusRtuFramer,
-    timeout=1
-)
+class Tester:
+    def __init__(self, port='/dev/ttyAMA3', baud=115200, timeout=1, timegap=12e-3):
+        self.port = port
+        self.baud = baud
+        self.timeout = timeout
+        self.timegap = timegap
+        self.client = ModbusSerialClient(
+            port=self.port,
+            baudrate=self.baud,
+            bytesize=8,
+            parity='N',
+            stopbits=1,
+            framer=ModbusRtuFramer,
+            timeout=self.timeout
+        )
+        assert (self.client.connect())
 
-print(icpdas)
-assert (icpdas.connect())
+    def tester(self):
+        while True:
+            # time
+            time.sleep(self.timegap)
+            ts = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            # esphome
+            di = \
+                self.client.read_holding_registers(
+                    slave=51, address=0, count=1)
+            print(f'{ts} {self.port} {"esphome":<8} di:{di}')
+            # drs power
+            try:
+                answ = self.client.read_input_registers(
+                    slave=131, address=0x50, count=1)
+                u = answ.registers[0] / 1e1
+            except AttributeError:
+                u = answ
+            try:
+                answ = self.client.read_input_registers(
+                    slave=131, address=0x60, count=3)
+                v, i, t = answ.registers
+                v /= 1e2
+                i /= 1e2
+                t /= 1e1
+            except AttributeError:
+                v = answ
+                i = t = ''
+            print(f'{ts} {self.port} {"drs":<8} u:{u} v:{v} i:{i} t:{t}')
 
-# icpdas.read_holding_registers(slave=131, address=0, count=1).registers
-# icpdas.read_holding_registers(slave=51, address=0, count=1).registers
 
-# icpdas.write_register(slave=131, address=0, value=0)  # off
-# icpdas.write_register(slave=131, address=0, value=1)  # on
+ama3 = Tester('/dev/ttyAMA3')
+usb = Tester('/dev/ttyUSB0')
+
+ama3.tester()
+# usb.tester()
+
+# # self.client.read_holding_registers(slave=131, address=0, count=1).registers
+# # self.client.read_holding_registers(slave=51, address=0, count=1).registers
+
+# # self.client.write_register(slave=131, address=0, value=0)  # off
+# # self.client.write_register(slave=131, address=0, value=1)  # on
 
 
-TIMEGAP = 11e-3
-print(f'TIMEGAP:{TIMEGAP*1e3}ms')
+# def fault_maker():
+#     while True:
+#         ts = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#         # DRS polling
+#         time.sleep(TIMEGAP)
+#         # ESPhome polling
+#         di = \
+#             self.client.read_holding_registers(
+#                 slave=51, address=0, count=1).registers[0]
+#         print(f'{ts} {"esphome":<8} di:{di}')
+#         time.sleep(TIMEGAP)
 
-def fault_maker():
-    while True:
-        ts = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        # DRS polling
-        u = icpdas.read_input_registers(
-            slave=131, address=0x50, count=1).registers[0]
-        u /= 1e1
-        v, i, t = \
-            icpdas.read_input_registers(
-                slave=131, address=0x60, count=3).registers
-        v /= 1e2
-        i /= 1e2
-        t /= 1e1
-        print(f'{ts} {"drs":<8} u:{u} v:{v} i:{i} t:{t}')
-        time.sleep(TIMEGAP)
-        # ESPhome polling
-        di = \
-            icpdas.read_holding_registers(
-                slave=51, address=0, count=1).registers[0]
-        print(f'{ts} {"esphome":<8} di:{di}')
-        time.sleep(TIMEGAP)
 
-fault_maker()
+# fault_maker()
