05a92b4dc8c86daf934e8d49c243f7ba5c0cb9a9
Dmitry Ponyatov <dponyatov@gmail.com>
2026-06-04 16:28:57 +0400

    .

--- ts/comment.ts
+++ ts/comment.ts
@@ -4,7 +4,6 @@
  * @author Dmitry Ponyatov <dponyatov@gmail.com>
  * @date 04/06/2026
  * @details Demonstrates line comments, block comments, and JSDoc/TSDoc
- *          documentation comments as used in Decl-to-TypeScript translation.
  */
 
 // line comment - single line comment inherited from C/C++
