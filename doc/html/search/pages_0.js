7b0bacafdfe806dd958aa626c8620a0740373ce4
Dmitry Ponyatov <dponyatov@gmail.com>
2026-07-03 21:20:26 +0400

    .

---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_21_5blogo_5d_5bvscode_2flogo_2epng_5d_20_3ctt_3edecl_3c_2ftt_3e_200_2e0_2e1_0',['![logo][vscode/logo.png] &lt;tt&gt;decl&lt;/tt&gt; 0.0.1',['../index.html',1,'']]]
+];
